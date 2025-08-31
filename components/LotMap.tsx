5e51c105199c2a4baae0dbec3935074cc5ac2ad4
Dmitry Stepanov <dmitry_stepanov@inbox.ru>
2025-08-31 21:02:21 +0300

    feat: add zoomControl and fullSscrenControl on Yandex maps

--- components/LotMap.tsx
+++ components/LotMap.tsx
@@ -14,14 +14,15 @@ export default function LotMap({ coordinates }: LotMapProps) {
       defaultState={{
         center: coordinates,
         zoom: 14,
-        // controls: [
-        //     "zoomControl",  // Отвечает за отображение кнопок приближения и отдаления (+ / -).
-        //     "fullscreenControl", // обавляет кнопку, которая позволяет развернуть карту на весь экран
-        //     // "geolocationControl", // Кнопка для определения местоположения пользователя
-        //     // "typeSelector", // Переключатель типа карты (схема, спутник, гибрид)
-        //     // "rulerControl" // Линейка для измерения расстояний
-        // ],
+        controls: [
+            "zoomControl",  // Отвечает за отображение кнопок приближения и отдаления (+ / -).
+            "fullscreenControl", // Добавляет кнопку, которая позволяет развернуть карту на весь экран
+            // "geolocationControl", // Кнопка для определения местоположения пользователя
+            // "typeSelector", // Переключатель типа карты (схема, спутник, гибрид)
+            // "rulerControl" // Линейка для измерения расстояний
+        ],
       }}
+      modules={["control.ZoomControl", "control.FullscreenControl"]}
       width="100%"
       height="400px"
     >
